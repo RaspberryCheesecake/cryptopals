a6a4d66cdb5ece15b7de686ca7cc8a9feeedf6b6
Hannah Hazi <hannah.hazi@paconsulting.com>
2016-12-01 14:44:52 +0000

    Added test to thoroughly check that every char converts neatly into a padded byte

--- testing_challenge1.cpp
+++ testing_challenge1.cpp
@@ -8,7 +8,30 @@ TEST_CASE("Single hex char turns into appropriate padded byte") {
 	REQUIRE(HexCharToByte(test_char) == result_byte);
 }
 
-TEST_CASE("Hex representation in chars is not converted to byte if not even") {
+TEST_CASE("Every hex char turns into the appropriate padded byte") {
+	const char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	const char lowers[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+	const char uppers[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+	const char rubbish[] = {'!', '=', '+', '?', '[', '}'};
+	
+	for (int i = 0; i < 6; i++) {
+        REQUIRE( HexCharToByte(lowers[i]) == i + 10 );
+        REQUIRE( HexCharToByte(uppers[i]) == i + 10 );
+        REQUIRE( HexCharToByte(rubbish[i]) == 0x00 );
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		REQUIRE( HexCharToByte(numbers[i]) == i );
+	}
+
+}
+
+TEST_CASE("Hex representation in pair of char is correctly converted to single byte") {
 	const char test_char_pair[] = {'a', 'b'};
 	REQUIRE(HexCharPairToBytes(test_char_pair) == 0xba);
+}
+
+TEST_CASE("Hex represenation of series of char values is correctly converted to bytes") {
+	//const char* test_saying = 'deadbeef11';
+	
 }
